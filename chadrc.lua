efd1d70ac4f107c59070c08407ed60c3b1a233b5
Sravan Suresh <53465522+srav001@users.noreply.github.com>
2023-05-12 19:09:32 +0530

    Update chadrc.lua

--- chadrc.lua
+++ chadrc.lua
@@ -1,7 +1,7 @@
 ---@type ChadrcConfig
 local M = {}
 
--- Path to overriding theme and highlights files
+vim.wo.relativenumber = true
 local highlights = require "custom.highlights"
 
 M.ui = {
@@ -10,15 +10,13 @@ M.ui = {
 
   hl_override = highlights.override,
   hl_add = highlights.add,
-  
+
   statusline = {
     theme = "vscode_colored",
   },
 }
 
 M.plugins = "custom.plugins"
-
--- check core.mappings for table structure
 M.mappings = require "custom.mappings"
 
 return M
